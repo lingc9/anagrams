bc3401492aae004631af2aa08fa61022bdf72ce9
Carol Ling <lingcarol9@gmail.com>
2022-11-07 09:28:10 -0500

    First implementation of anagrams.py.

--- anagrams.py
+++ anagrams.py
@@ -1,1 +1,19 @@
-# Your driver code (including input / output handling) should go here
+"""The driver code for find_anagrams.py, including input / output handling."""
+
+from find_anagrams import make_anagrams_dict
+import sys
+
+
+def convert_to_list(file_path):
+    word_list = [file_path]
+    return word_list
+
+
+def output(word_list):
+    anagrams_dict = make_anagrams_dict(word_list)
+    for key in anagrams_dict.keys():
+        print(anagrams_dict[key])
+
+
+list_1 = input("./" + str(sys.argv[1]))
+print(output(list_1))
